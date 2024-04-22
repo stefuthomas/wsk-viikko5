da194c1e5341403b48f5aa20385fd7bd67f8852f
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2024-04-22 10:52:57 +0300

    exercise ready

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom';
 const MediaRow = ({ item }) => (
   <tr>
     <td>
-      <img src={item.thumbnail} alt={item.title} />
+      <img src={item.thumbnail} alt={item.title}/>
     </td>
+    <td>{item.username}</td>
     <td>{item.title}</td>
     <td>{item.description}</td>
     <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
     <td>{item.filesize}</td>
     <td>{item.media_type}</td>
     <td>
-      <Link to={`/media/${item.media_id}`} state={{ item }}>View linkki</Link>
+      <Link to={`/media/${item.media_id}`} state={{item}}>View linkki</Link>
     </td>
   </tr>
 );
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -8,8 +8,14 @@ const Home = () => {
 
 
   const getMedia = async () => {
-      const mediaResult = await fetchData('test.json');
-      setMediaArray(mediaResult);
+      const mediaResult = await fetchData(import.meta.env.VITE_MEDIA_API + '/media');
+
+
+      const mediaWithUser = await Promise.all (mediaResult.map(async (item) => {
+        const userResult = await fetchData(import.meta.env.VITE_AUTH_API + '/users/' + item.user_id);
+        return {...item, username: userResult.username};
+      }));
+    setMediaArray(mediaWithUser);
   }
 
   useEffect(() => {
@@ -24,6 +30,7 @@ const Home = () => {
         <thead>
         <tr>
           <th>Thumbnail</th>
+          <th>User</th>
           <th>Title</th>
           <th>Description</th>
           <th>Created</th>
