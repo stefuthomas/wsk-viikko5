179ed14ba766fd09cdad4507c3918afd5c207ff6
stefuthomas <142782594+stefuthomas@users.noreply.github.com>
2024-04-22 12:33:15 +0300

    exercise ready

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Single from './views/Single';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
